0d15413942af8f5d58020353046bfd0971c7a5f1
Kim-Maximilian Nürnberger <git@kim-nuernberger.eu>
2015-12-25 17:45:48 +0100

    Added a simple UDP Command sender project

---
+++ src/FlyingDMX - SimpleSender/SimpleSender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Nuernberger.FlyingDMX
+{
+    public class SimpleSender
+    {
+        private UdpClient sender;
+        private IPEndPoint endpoint;
+
+        public SimpleSender(int port)
+        {
+            sender = new UdpClient();
+            endpoint = new IPEndPoint(IPAddress.Parse("255.255.255.255"), port);
+        }
+
+        public void SendCommand(Command cmd)
+        {
+            SendLine(cmd.ToString());
+        }
+
+        public void SendLine(string text)
+        {
+            byte[] data = Encoding.ASCII.GetBytes("Hello UDP Server!!!");
+            sender.Send(data, data.Length, endpoint);
+            sender.Close();
+        }
+    }
+}
